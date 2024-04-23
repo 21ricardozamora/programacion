15e70f12d47ac9ebb0225b84f201c1f8e8bfa68d
ricardo <ultraaa_ricky@hotmaol.com>
2024-04-23 23:54:13 +0100

    se crea la tarea

---
+++ fraction.py
@@ -0,0 +1,44 @@
+from __future__ import annotations
+
+
+class Fraction:
+    # def gcd(a: int, b: int) -> int:
+    #    """Euclid's Algorithm"""
+    #    while b > 0:
+    #        a, b = b, a % b
+    #    return a
+
+    def __init__(self, num: int, den: int):
+        div = Fraction.gcd(num, den)
+        self.num = num / div
+        self.den = den / div
+
+    def __add__(self, other: Fraction):
+        num = (self.num * other.den) + (self.den * other.num)
+        den = self.den * other.den
+        return Fraction(num, den)
+
+    def __sub__(self, other: Fraction):
+        num = (self.num * other.den) - (self.den * other.num)
+        den = self.den * other.den
+        return Fraction(num, den)
+
+    def __mul__(self, other: Fraction):
+        num = self.num * other.num
+        den = self.den * other.den
+        return Fraction(num, den)
+
+    def __truediv__(self, other: Fraction):
+        num = self.num * other.den
+        den = self.den * other.num
+        return Fraction(num, den)
+
+    def __str__(self) -> str:
+        pass
+
+    @staticmethod
+    def gcd(a: int, b: int) -> int:
+        """Euclid's Algorithm"""
+        while b > 0:
+            a, b = b, a % b
+        return a
