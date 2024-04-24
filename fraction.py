20849403c5ef2c3fb2f873f2b2937eb06118a4be
21ricardozamora <zamoraricardo21288@gamil.com>
2024-04-24 15:18:02 +0100

    se modifica el codigo

--- fraction.py
+++ fraction.py
@@ -2,39 +2,34 @@ from __future__ import annotations
 
 
 class Fraction:
-    # def gcd(a: int, b: int) -> int:
-    #    """Euclid's Algorithm"""
-    #    while b > 0:
-    #        a, b = b, a % b
-    #    return a
 
     def __init__(self, num: int, den: int):
         div = Fraction.gcd(num, den)
-        self.num = num / div
-        self.den = den / div
+        self.num = num // div
+        self.den = den // div
 
-    def __add__(self, other: Fraction):
+    def __add__(self, other: Fraction) -> Fraction:
         num = (self.num * other.den) + (self.den * other.num)
         den = self.den * other.den
         return Fraction(num, den)
 
-    def __sub__(self, other: Fraction):
+    def __sub__(self, other: Fraction) -> Fraction:
         num = (self.num * other.den) - (self.den * other.num)
         den = self.den * other.den
         return Fraction(num, den)
 
-    def __mul__(self, other: Fraction):
+    def __mul__(self, other: Fraction) -> Fraction:
         num = self.num * other.num
         den = self.den * other.den
         return Fraction(num, den)
 
-    def __truediv__(self, other: Fraction):
+    def __truediv__(self, other: Fraction) -> Fraction:
         num = self.num * other.den
         den = self.den * other.num
         return Fraction(num, den)
 
     def __str__(self) -> str:
-        pass
+        return f'{self.num}/{self.den}'
 
     @staticmethod
     def gcd(a: int, b: int) -> int:
